171329539208671701585f548f18892b9eed73d2
rizqimaulanaa <rizqi2204@gmail.com>
2019-12-26 22:12:06 +0700

    Add files via upload

---
+++ random.php
@@ -0,0 +1,37 @@
+<?php 
+@ini_set('output_buffering',0);
+$dasar = '0812';
+function GeneRate($qtd){ 
+$karakter = mt_rand(100,999);
+$tykung = strlen($karakter); 
+$tykung--; 
+$Hash=NULL; 
+    for($x=0;$x<=$qtd;$x++){ 
+        $Posicao = rand(0,$tykung); 
+        $Hash .= substr($karakter,$Posicao,3); 
+    } 
+return $Hash; 
+} 
+print "EXTRAP GENERATOR\n";
+print "Author: Rzqmln\n";
+print "Suport By : AnonCyberTeam || Indosec\n";
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+echo "$dasar"; echo GeneRate(7);
+print "\n";
+print "\n";
